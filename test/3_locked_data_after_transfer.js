52e31d586da0e79168e81c358bce296e9d341816
Ivan Solomichev <vanijatko.social@gmail.com>
2019-10-24 14:43:58 +0300

    getMyReleaseTimestamps tested

--- test/3_locked_data_after_transfer.js
+++ test/3_locked_data_after_transfer.js
@@ -33,7 +33,7 @@ contract("Locked data after transfer", (_accounts) => {
     await etheousToken.transfer(USER_0_ADDRESS, USER_0_TOKENS_RECEIVED, USER_0_LOCK_DURATION, USER_0_LOOP_ITERATIONS);
   });
 
-  describe.only("getReleaseTimestamps", async () => {
+  describe("getReleaseTimestamps", async () => {
     it("should update releaseTimestamps after initial transfer", async () => {
       assert.equal((await etheousToken.getReleaseTimestamps.call(USER_0_ADDRESS)).length, 1, "wrong release timestamps count after 0");
       assert.equal((await etheousToken.getReleaseTimestamps.call(USER_0_ADDRESS))[0].toNumber(), user0ReleaseTimestamp_0.toNumber(), "wrong release timestamps[0] after 0");
@@ -64,6 +64,62 @@ contract("Locked data after transfer", (_accounts) => {
       assert.equal((await etheousToken.getReleaseTimestamps.call(USER_0_ADDRESS))[0].toNumber(), user0ReleaseTimestamp_0.toNumber(), "wrong release timestamps[0] after 2");
       assert.equal((await etheousToken.getReleaseTimestamps.call(USER_0_ADDRESS))[1].toNumber(), user0ReleaseTimestamp_1.toNumber(), "wrong release timestamps[1] after 2");
       assert.equal((await etheousToken.getReleaseTimestamps.call(USER_0_ADDRESS))[2].toNumber(), user0ReleaseTimestamp_2.toNumber(), "wrong release timestamps[2] after 2");
+    });
+  });
+
+  describe("getMyReleaseTimestamps", async () => {
+    it("should update getMyReleaseTimestamps after initial transfer", async () => {
+      assert.equal((await etheousToken.getMyReleaseTimestamps.call({
+        from: USER_0_ADDRESS
+      })).length, 1, "wrong release timestamps count after 0");
+      assert.equal((await etheousToken.getMyReleaseTimestamps.call({
+        from: USER_0_ADDRESS
+      }))[0].toNumber(), user0ReleaseTimestamp_0.toNumber(), "wrong release timestamps[0] after 0");
+    });
+
+    it("should update getMyReleaseTimestamps after multiple transfers", async () => {
+      //  0
+      assert.equal((await etheousToken.getMyReleaseTimestamps.call({
+        from: USER_0_ADDRESS
+      })).length, 1, "wrong release timestamps count after 0");
+      assert.equal((await etheousToken.getMyReleaseTimestamps.call({
+        from: USER_0_ADDRESS
+      }))[0].toNumber(), user0ReleaseTimestamp_0.toNumber(), "wrong release timestamps[0] after 0");
+
+      //  1
+      const USER_0_TOKENS_RECEIVED_1 = ether("0.2");
+      const USER_0_LOCK_DURATION_1 = time.duration.minutes(2);
+      await etheousToken.transfer(USER_0_ADDRESS, USER_0_TOKENS_RECEIVED_1, USER_0_LOCK_DURATION_1, USER_0_LOOP_ITERATIONS);
+      let user0ReleaseTimestamp_1 = new BN(await time.latest()).add(USER_0_LOCK_DURATION_1);
+
+      assert.equal((await etheousToken.getMyReleaseTimestamps.call({
+        from: USER_0_ADDRESS
+      })).length, 2, "wrong release timestamps count after 1");
+      assert.equal((await etheousToken.getMyReleaseTimestamps.call({
+        from: USER_0_ADDRESS
+      }))[0].toNumber(), user0ReleaseTimestamp_0.toNumber(), "wrong release timestamps[0] after 1");
+      assert.equal((await etheousToken.getMyReleaseTimestamps.call({
+        from: USER_0_ADDRESS
+      }))[1].toNumber(), user0ReleaseTimestamp_1.toNumber(), "wrong release timestamps[1] after 1");
+
+      //  2
+      const USER_0_TOKENS_RECEIVED_2 = ether("0.3");
+      const USER_0_LOCK_DURATION_2 = time.duration.minutes(3);
+      await etheousToken.transfer(USER_0_ADDRESS, USER_0_TOKENS_RECEIVED_2, USER_0_LOCK_DURATION_2, USER_0_LOOP_ITERATIONS);
+      let user0ReleaseTimestamp_2 = new BN(await time.latest()).add(USER_0_LOCK_DURATION_2);
+
+      assert.equal((await etheousToken.getMyReleaseTimestamps.call({
+        from: USER_0_ADDRESS
+      })).length, 3, "wrong release timestamps count after 2");
+      assert.equal((await etheousToken.getMyReleaseTimestamps.call({
+        from: USER_0_ADDRESS
+      }))[0].toNumber(), user0ReleaseTimestamp_0.toNumber(), "wrong release timestamps[0] after 2");
+      assert.equal((await etheousToken.getMyReleaseTimestamps.call({
+        from: USER_0_ADDRESS
+      }))[1].toNumber(), user0ReleaseTimestamp_1.toNumber(), "wrong release timestamps[1] after 2");
+      assert.equal((await etheousToken.getMyReleaseTimestamps.call({
+        from: USER_0_ADDRESS
+      }))[2].toNumber(), user0ReleaseTimestamp_2.toNumber(), "wrong release timestamps[2] after 2");
     });
   });
 
